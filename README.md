96713b6b9819e6be742576f4a78b41b398bda623
abnerAmos <abner.fsouza1@gmail.com>
2022-10-13 17:34:44 -0300

    Exercicio Porcentagem com BigDecimal

---
+++ README.md
@@ -0,0 +1,6 @@
+# logic-practice
+Praticando Algoritmos
+
+###Exercicio 1:
+- Faça um programa que peça um valor monetário e diminua-o
+em 15% e imprima o resultado (Com BigDecimal).
---
+++ src/main/java/com/exercice/logic/Exercicios/Ex1.java
@@ -0,0 +1,24 @@
+package com.exercice.logic.Exercicios;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Ex1 {
+
+    // Informe um valor monetário e diminua 15 porcento do valor, com BigDecimal
+
+    public static void main(String[] args) {
+
+        System.out.println("Informe um valor");
+        String input = new Scanner(System.in).next();
+
+        BigDecimal valueIn = new BigDecimal(input);
+        BigDecimal percentage = new BigDecimal("15");
+        BigDecimal division = new BigDecimal("100");
+
+        BigDecimal result = valueIn.multiply(percentage).divide(division);
+        BigDecimal total = valueIn.subtract(result);
+
+        System.out.println("Novo valor: " + total);
+    }
+}
