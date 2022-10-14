f7e895c85779070829c874b4872fdfc17c0d4c40
abnerAmos <abner.fsouza1@gmail.com>
2022-10-14 16:34:06 -0300

    Exercicio Pergunta e Resposta

--- README.md
+++ README.md
@@ -3,4 +3,13 @@ Praticando Algoritmos
 
 ###Exercicio 1:
 - Faça um programa que peça um valor monetário e diminua-o
-em 15% e imprima o resultado (Com BigDecimal).
+em 15% e imprima o resultado (Com BigDecimal)
+
+###Exercicio 2:
+-  Faça um programa que leia a validade das informações:<br>
+   <br>a. Idade: entre 0 e 150;
+   <br>b. Salário: maior que 0;
+   <br>c. Sexo: M, F ou Outro;<br>
+   <br>O programa deve imprimir uma mensagem de erro para cada informação
+   inválida.
+
---
+++ src/main/java/com/exercice/logic/Exercicios/Ex2.java
@@ -0,0 +1,33 @@
+package com.exercice.logic.Exercicios;
+
+import java.util.Scanner;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Informe uma Idade:");
+        Scanner ageIn = new Scanner(System.in);
+        Integer age = ageIn.nextInt();
+
+        System.out.println("Informe um Salario:");
+        Scanner salaryIn = new Scanner(System.in);
+        Double salary = salaryIn.nextDouble();
+
+        System.out.println("Informe um Sexo:");
+        Scanner genderIn = new Scanner(System.in);
+        String gender = genderIn.next();
+
+        String returnAge = (age > 150) ? "Não esta mais vivo" : age.toString();
+        System.out.println(returnAge);
+
+        String retornSalary = (salary > 0) ? "Sem dinheiro em caixa" : salary.toString();
+        System.out.println(retornSalary);
+
+        if(!(gender.equals("M") || gender.equals("F") || gender.equals("Outro")))
+            System.out.println("Genero Inválido");
+        else System.out.println(gender);
+
+
+    }
+}
